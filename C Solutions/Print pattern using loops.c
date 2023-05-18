38823c4df22e9ea2ab17291fe1b3d46de21fda38
Tinu205 <dazleroger404@gmail.com>
2023-05-18 20:28:53 +0530

    Need to update the loop logic

--- C Solutions/Print pattern using loops.c
+++ C Solutions/Print pattern using loops.c
@@ -1,21 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,end_val;
-    //temp
-    n = 4;
+    int n,end_val,x ,y;
+    //Initialising the input cuz i'm lazy
+
+    n = 3;
     end_val = (n*2)-1;
-    for(int i=1;i<=n;i++){
-        int tmpval = n;
-        for(int j = 1;j<=i;j++){
-            printf("%d ",tmpval);
-            tmpval-=1;
-        }printf("\n");
+
+
+    for(int i = 1; i <= end_val;i++){
+
+        for(int j = 1;j <= end_val;j++){
+
+            if(i ==1 || j==1 || i == end_val || j==end_val){
+                printf("%d ",n);
+            }else if(i ==2 || j==2 || i == end_val-1 || j==end_val-1){
+                printf("%d ",n-1);
+            }else{
+                printf("%d ",1);
+            }
+
+        }
+        printf("\n");
     }
-    for(int i=1;i<=n;i++){
-        for(int j = n;j>i;j--){
-            printf("%d ",j);
-        }printf("\n");
-    }//incomplete 
     
 }
