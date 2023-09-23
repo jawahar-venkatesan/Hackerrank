af6d39bdd6f1de32be8556055e8be34324fda05a
Tinu205 <dazleroger404@gmail.com>
2023-09-23 19:28:34 +0530

    Added features

--- Java Solutions/java02.java
+++ Java Solutions/java02.java
@@ -4,7 +4,11 @@ public class java02 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println(a);
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
