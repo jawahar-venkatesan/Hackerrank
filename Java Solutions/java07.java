fa1a8085b33efb54888d31894a4d44cd72279d84
Tinu205 <dazleroger404@gmail.com>
2023-11-29 18:27:53 +0530

    Added java07.java

---
+++ Java Solutions/java07.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class java07{
+  public static void main(String []argh){
+    Scanner in = new Scanner (System.in);
+    int t = in.nextInt();
+    for(int i =0;i<t;i++){
+      int a = in.nextInt();
+      int b = in.nextInt();
+      int c = in.nextInt();
+    }
+    in.close();
+
+  }
+}
