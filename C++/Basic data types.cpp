7dda16d2f970d839f3fe50ecf3b1395905b48569
Tinu205 <dazleroger404@gmail.com>
2023-12-28 11:05:46 +0530

    Added initial c++ files

---
+++ C++/Basic data types.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdio>
+
+int main(){
+    int i;
+    long l;
+    char a;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf",&i,&l,&a,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf",i,l,a,f,d);
+}
